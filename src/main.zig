1695bd4bb3d3d188cbabc5ca9c7986306e638044
gz <sanshanliang3@gmail.com>
2022-11-27 21:57:18 +0900

    hw

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,8 @@
-const std = @import("std");
+const c = @cImport({
+    @cInclude("stdio.h");
+});
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
-
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+pub fn main() u8 {
+    _ = c.printf("hello world!\n");
+    return 0;
 }
